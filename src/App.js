73efd419e3e4a2dd5c9997a93bc47ffe81f0b721
pipe473 <bedoyafelipe743@gmail.com>
2020-06-23 18:09:56 +0200

    Agregando react roter dom y componentes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Header from './components/Header';
+import Productos from './components/Productos';
+import NuevoProducto from './components/NuevoProducto';
+import EditarProducto from './components/EditarProducto';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
@@ -7,6 +10,13 @@ function App() {
   return (
     <Router>
       <Header />
+      <div className="container mt-5">
+        <Switch>
+          <Route exact path="/" component={Productos} />
+          <Route exact path="/productos/nuevo" component={NuevoProducto} />
+          <Route exact path="/productos/editar/:id" component={EditarProducto} />
+        </Switch>
+      </div>
     </Router>
   );
 }
