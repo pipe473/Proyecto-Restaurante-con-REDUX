ccb285134b800d90dd5bda2f5aab093880f6ff55
pipe473 <bedoyafelipe743@gmail.com>
2020-06-24 20:49:13 +0200

    Configurando el Action para listar los productos

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -2,6 +2,9 @@ import {
   AGREGAR_PRODUCTO,
   AGREGAR_PRODUCTO_EXITO,
   AGREGAR_PRODUCTO_ERROR,
+  COMENZAR_DESCARGA_PRODUCTOS,
+  DESCARGA_PRODUCTOS_EXITO,
+  DESCARGA_PRODUCTOS_ERROR
 } from "../types";
 import clienteAxios from "../config/axios";
 import Swal from 'sweetalert2';
@@ -56,3 +59,15 @@ const agregarProductoError = (estado) => ({
   type: AGREGAR_PRODUCTO_ERROR,
   payload: estado,
 });
+
+// Función que descarga los productos de la base de datos
+export function obtenerProductosAction() {
+  return async (dispatch) => {
+    dispatch( descargarProductos() );
+  }
+}
+
+const descargarProductos = () => ({
+  type: COMENZAR_DESCARGA_PRODUCTOS,
+  payload: true
+})
