1e366a2108f37a49530beef57769e0cddf849ef3
pipe473 <bedoyafelipe743@gmail.com>
2020-06-24 19:14:26 +0200

    Administrando un posible error

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -29,6 +29,7 @@ const agregarProductoExito = producto => ({
 });
 
 // Si ha habido un error
-const agregarProductoError = () => ({
-
+const agregarProductoError = estado => ({
+    type: AGREGAR_PRODUCTO_ERROR,
+    payload: estado
 })
