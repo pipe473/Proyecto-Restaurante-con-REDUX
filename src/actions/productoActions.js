03ef4d75225bb28f48f9c57f21898827ea83a976
pipe473 <bedoyafelipe743@gmail.com>
2020-06-24 21:09:42 +0200

    Obteniendo los productos en el state

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -64,10 +64,28 @@ const agregarProductoError = (estado) => ({
 export function obtenerProductosAction() {
   return async (dispatch) => {
     dispatch( descargarProductos() );
+
+    try {
+      const respuesta = await clienteAxios.get('/productos');
+      dispatch( descargaProductosExitosa(respuesta.data) );
+      
+    } catch (error) {
+      console.log(error);
+      
+      dispatch( descargaProductosError() );
+    }
   }
 }
 
 const descargarProductos = () => ({
   type: COMENZAR_DESCARGA_PRODUCTOS,
+  payload: true
+});
+const descargaProductosExitosa = productos => ({
+    type: DESCARGA_PRODUCTOS_EXITO,
+    payload: productos
+});
+const descargaProductosError = pro => ({
+  type: DESCARGA_PRODUCTOS_ERROR,
   payload: true
 })
